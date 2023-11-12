bed1ec71bcd900ee53e107608166d4bc3bd40cea
Juanqu <jfquintero261@gmail.com>
2023-11-12 09:59:54 -0500

    comprarTiquete template

--- Chivas/app/urls.py
+++ Chivas/app/urls.py
@@ -3,5 +3,6 @@ from app import views
 
 urlpatterns = [
     path('',views.index,name="index2"),
-    # path('',views.css,name="style")
+    path('comprarTiquete/<int:viaje_id>/',views.comprarTiquete, name="comprarTiquete"),
+    path('cantidadTiquetes/', views.cantidadTiquetes, name='cantidadTiquetes'),
 ]
--- Chivas/app/views.py
+++ Chivas/app/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Viajes
 
 # Create your views here.
 def index(request):
-    return render(request, "index2.html", {
-        'viajes' : Viajes.objects.all()
-    })
+    viajes = Viajes.objects.all()
+    return render(request, 'index2.html', {'viajes': viajes})
+
+def comprarTiquete(request, viaje_id):
+    viaje = get_object_or_404(Viajes, id=viaje_id)
+    return render(request, 'comprarTiquete.html', {'viaje': viaje})
+
+def cantidadTiquetes(request):
+    viaje_id = request.POST.get('viaje_id')  
+    viaje = get_object_or_404(Viajes, pk=viaje_id)
+    if request.method == 'POST':
+        selected_value = request.POST.get('selectField', None)
+        return render(request, 'comprarTiquete.html', {'selected_value': selected_value, 'viaje':viaje})
+    return render(request, 'comprarTiquete.html', {'selected_value': None})
